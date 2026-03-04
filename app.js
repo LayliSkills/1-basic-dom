4a8c2c48e0e0e863a9ecea8e6e78b137b60d29dd
LayliSkills <layliskills@gmail.com>
2026-03-04 14:56:05 +0300

    added new file of css

--- app.js
+++ app.js
@@ -4,8 +4,20 @@
 // // get element by class name
 // let msg2 = document.getElementsByClassName("message");
 // console.log(msg2);
+// let msg3 = document.getElementsByClassName("message");
+// console.log(msg3);
 
 
 // using input field  selecting by name
-let inpt = document.getElementsByName("language");
-console.log(inpt);
+// let inpt = document.getElementsByName("language");
+// console.log(inpt);
+
+// using get element by tag name
+// let cont = document.getElementById("container");
+
+// let heading = cont.getElementsByTagName("h1");
+// console.log(heading);
+
+let msg = document.querySelector(".message");
+console.log(msg);
+
