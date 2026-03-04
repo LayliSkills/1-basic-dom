22cbaa22763c927a4e5fb7ac1e1307d83fb15c42
LayliSkills <layliskills@gmail.com>
2026-03-04 15:33:13 +0300

    adding more selecotr and query

--- app.js
+++ app.js
@@ -18,6 +18,23 @@
 // let heading = cont.getElementsByTagName("h1");
 // console.log(heading);
 
-let msg = document.querySelector(".message");
-console.log(msg);
+// let msg = document.querySelector(".message");
+// console.log(msg);
+
+// let doc = document.querySelectorAll(".pages");
+// console.log(doc);
+
+
+// reversgin elements
+// let txt = document.querySelector(".text");
+// console.log(txt.parentNode);
+//  let con = document.querySelector(".container");
+//  console.log(con.lastElementChild);
+//  console.log(con.childNodes);
+ 
+let div = document.createElement("div");
+div.innerHTML = "<h1>Hello World</h1>";
+document.body.appendChild(div);
+console.log(div);
+
 
