474ad3d605da789ce7bc421f0f46e23fccd450dc
LayliSkills <layliskills@gmail.com>
2026-03-04 14:40:42 +0300

    added some files of selection

---
+++ app.js
@@ -0,0 +1,11 @@
+// get element by id
+// let msg = document.getElementById("message");
+// console.log(msg);
+// // get element by class name
+// let msg2 = document.getElementsByClassName("message");
+// console.log(msg2);
+
+
+// using input field  selecting by name
+let inpt = document.getElementsByName("language");
+console.log(inpt);
